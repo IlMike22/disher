90c277c8a246050409c13486d3e3de76b98b352a
IlMike22 <michaelwidlok@gmail.com>
2023-05-18 11:02:41 +0200

    adding a lil bit more form to details screen

--- app/src/main/java/com/example/disher/detail/presentation/DetailScreen.kt
+++ app/src/main/java/com/example/disher/detail/presentation/DetailScreen.kt
@@ -1,23 +1,30 @@
 package com.example.disher.detail.presentation
 
-import android.widget.ImageView
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberAsyncImagePainter
@@ -29,6 +36,7 @@ fun DetailScreen(
     mealId: String
 ) {
     val state by remember { viewModel.mealState }
+    val uriHandler = LocalUriHandler.current
 
     DisposableEffect(key1 = Unit) {
         if (mealId.isNotBlank()) {
@@ -36,9 +44,11 @@ fun DetailScreen(
         }
         onDispose {}
     }
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(16.dp)) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
         if (state.isLoading) {
             Text(text = "LOADING")
         } else if (state.isError) {
@@ -49,7 +59,7 @@ fun DetailScreen(
                 style = MaterialTheme.typography.headlineLarge
             )
             Spacer(modifier = Modifier.height(16.dp))
-            Row() {
+            Row {
                 Text(
                     text = state.mealDetails?.strCategory ?: "",
                     style = MaterialTheme.typography.headlineSmall
@@ -65,8 +75,56 @@ fun DetailScreen(
             Spacer(modifier = Modifier.height(32.dp))
             Text(
                 text = state.mealDetails?.strIngredient1 ?: "",
-                style = MaterialTheme.typography.bodySmall
+                style = MaterialTheme.typography.bodyMedium
             )
+            Spacer(modifier = Modifier.height(8.dp))
+            if (state.mealDetails?.strYoutube != null) {
+                ClickableText(
+                    style = MaterialTheme.typography.bodyMedium,
+                    text = AnnotatedString(text = "Find more info on Youtube"),
+                    onClick = {
+                        uriHandler.openUri(state.mealDetails?.strYoutube ?: "")
+                    }
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            InstructionTextBlock(state.mealDetails?.strInstructions ?: "")
         }
     }
 }
+
+@Composable
+fun InstructionTextBlock(
+    instructions: String,
+    modifier: Modifier = Modifier
+) {
+    var showMore by remember {
+        mutableStateOf(false)
+    }
+
+    Column(modifier = modifier
+        .padding(16.dp)
+        .fillMaxWidth()) {
+        Box(modifier = Modifier
+            .`if`(!showMore) {
+                height(100.dp)
+            }
+        ) {
+            Text(text = instructions, style = MaterialTheme.typography.bodySmall)
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = { showMore = !showMore }) {
+            Text(text = if (!showMore) "Show more..." else "Show less..")
+        }
+    }
+}
+
+fun Modifier.`if`(
+    condition: Boolean,
+    then: Modifier.() -> Modifier
+): Modifier =
+    if (condition) {
+        then()
+    } else {
+        this
+    }
